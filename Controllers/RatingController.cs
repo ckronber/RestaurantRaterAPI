22dea983b26a4ab80f41c19fc6af947ae3c8b19c
Chris Kronberg <ckronber@gmail.com>
2021-05-25 22:08:03 -0400

    update: UpdateRatings was added to RatingController

--- Controllers/RatingController.cs
+++ Controllers/RatingController.cs
@@ -38,26 +38,87 @@ namespace RestaurantRaterAPI.Controllers
 
         //GetRatingsbyRestaurantID
         [HttpGet]
-        public async Task<IHttpActionResult> GetRatingById(int RestaurantID)
+        public async Task<IHttpActionResult> GetRatingByRestaurantId(int Id)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
 
-            Restaurant restaurant = await _context.Restaurants.FindAsync(RestaurantID);
+            Restaurant restaurant = await _context.Restaurants.FindAsync(Id);
 
             if (restaurant == null)
             {
-                return BadRequest($"The target restaurant with the Id of {RestaurantID} does not exist");
+                return BadRequest($"The target restaurant with the Id of {Id} does not exist");
             }
-            else
+
+            return Ok(restaurant.Ratings);
+        }
+
+        //GetRatingsbyRestaurantID
+        /*
+        [HttpGet]
+        public async Task<IHttpActionResult> GetRatingByRestaurauntAndRatingIDs(int Id)
+        {
+            if (!ModelState.IsValid)
             {
-                return Ok(restaurant.Ratings);
+                return BadRequest();
+            }
+
+            Restaurant restaurant = await _context.Restaurants.FindAsync(Id);
+            
+            if (restaurant == null)
+            {
+                return BadRequest($"The target restaurant with the Id of {Id} does not exist");
+            }
+
+            foreach(Rating model in restaurant.Ratings)
+            {
+                if (model.Id == restaurant)
+                    return Ok(model);
+            }
+
+            return InternalServerError();
+        }
+        */
+        //UpdateRating
+
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRatingByRestaurantId(int Id,Rating model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Restaurant restaurant = await _context.Restaurants.FindAsync(model.RestaurantId);
+
+            if(restaurant == null)
+            {
+                return BadRequest("");
+            }
+
+            foreach (Rating models in restaurant.Ratings)
+            {
+                if (models.Id == model.Id)
+                {
+                    models.FoodScore = model.FoodScore;
+                    models.EnvironmentScore = model.EnvironmentScore;
+                    models.CleanlinessScore = model.CleanlinessScore;
+
+                    if(await _context.SaveChangesAsync() == 1) 
+                        return Ok($"You successfully updated a rating for {restaurant.Name}");
+                }
             }
             return InternalServerError();
         }
-        //UpdateRating
+      /*
         //DeleteRating
-    }
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurantId(int Id)
+        {
+
+        }
+      */
+    }   
 }
