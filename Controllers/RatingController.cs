86e83eb8fe187be031db54ddb4e893cdc05b91e1
Chris Kronberg <ckronber@gmail.com>
2021-05-25 23:48:48 -0400

    update: Update and delete  of Ratings are working for RatingController

--- Controllers/RatingController.cs
+++ Controllers/RatingController.cs
@@ -55,70 +55,62 @@ namespace RestaurantRaterAPI.Controllers
             return Ok(restaurant.Ratings);
         }
 
-        //GetRatingsbyRestaurantID
-        /*
-        [HttpGet]
-        public async Task<IHttpActionResult> GetRatingByRestaurauntAndRatingIDs(int Id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
-
-            Restaurant restaurant = await _context.Restaurants.FindAsync(Id);
-            
-            if (restaurant == null)
-            {
-                return BadRequest($"The target restaurant with the Id of {Id} does not exist");
-            }
-
-            foreach(Rating model in restaurant.Ratings)
-            {
-                if (model.Id == restaurant)
-                    return Ok(model);
-            }
-
-            return InternalServerError();
-        }
-        */
         //UpdateRating
-
         [HttpPut]
         public async Task<IHttpActionResult> UpdateRatingByRestaurantId(int Id,Rating model)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest();
+                return BadRequest("Not a valid Request");
             }
 
-            Restaurant restaurant = await _context.Restaurants.FindAsync(model.RestaurantId);
+            Restaurant restaurant = await _context.Restaurants.FindAsync(Id);
+            Rating rating = await _context.Ratings.FindAsync(model.Id);
 
             if(restaurant == null)
             {
-                return BadRequest("");
+                return BadRequest("Restaurant Not Found");
             }
 
-            foreach (Rating models in restaurant.Ratings)
+            if(rating.Id == model.Id)
             {
-                if (models.Id == model.Id)
-                {
-                    models.FoodScore = model.FoodScore;
-                    models.EnvironmentScore = model.EnvironmentScore;
-                    models.CleanlinessScore = model.CleanlinessScore;
+                rating.FoodScore = model.FoodScore;
+                rating.EnvironmentScore = model.EnvironmentScore;
+                rating.CleanlinessScore = model.CleanlinessScore;
 
-                    if(await _context.SaveChangesAsync() == 1) 
-                        return Ok($"You successfully updated a rating for {restaurant.Name}");
-                }
+                if (await _context.SaveChangesAsync() == 1)
+                    return Ok($"You successfully updated a rating for {restaurant.Name}");
             }
+         
             return InternalServerError();
         }
-      /*
+      
         //DeleteRating
         [HttpDelete]
-        public async Task<IHttpActionResult> DeleteRestaurantId(int Id)
+        public async Task<IHttpActionResult> DeleteRestaurantId(int Id,Rating model)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Not a valid Request");
+            }
 
+            Restaurant restaurant = await _context.Restaurants.FindAsync(Id);
+
+            if (restaurant == null)
+            {
+                return BadRequest("Restaurant Not Found");
+            }
+
+            Rating rating = await _context.Ratings.FindAsync(model.Id);
+            rating.RestaurantId = restaurant.Id;
+            
+            _context.Ratings.Remove(rating);
+
+            if (await _context.SaveChangesAsync() == 1)
+                return Ok($"Rating #{rating.Id} for {restaurant.Name} was successfully deleted.");
+
+            return InternalServerError();
         }
-      */
+      
     }   
 }
